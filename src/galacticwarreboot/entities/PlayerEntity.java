f4d808b6377eb47f9ee35ede53f59b506c64d2ef
Gary Fred Giger <ggiger@Bender.(none)>
2014-01-14 20:31:51 -0500

    I forgot to add the powerup and attribute classes required for the players thrust. I also cleaned up the PlayerEntity class by removing old commented code.

--- src/galacticwarreboot/entities/PlayerEntity.java
+++ src/galacticwarreboot/entities/PlayerEntity.java
@@ -3,7 +3,6 @@ package galacticwarreboot.entities;
 import java.awt.image.ImageObserver;
 
 import galacticwarreboot.Constants;
-import galacticwarreboot.Constants.AttributeType;
 import galacticwarreboot.attributes.PlayerAttributes;
 import game.framework.entities.EntityImage;
 import game.framework.primitives.Position2D;
@@ -11,13 +10,6 @@ import game.framework.utilities.GameEngineConstants;
 
 public class PlayerEntity extends EntityImage
 {
-//  private int firePower; 
-//  private int health;
-//  private int shield;
-//  private int superShield;  // The count of this indicates how many super shiled powerups the player current has
-//  private int theBomb;
-//  private boolean autoShield;
-
   private PlayerAttributes attributes;
   private Position2D homePosition; 
 
@@ -85,169 +77,10 @@ public class PlayerEntity extends EntityImage
     return attributes.isEquipped(powerupType);
   }
   
-//  /*
-//   * Methods regarding player's health
-//   */
-//  public void incrementHealthAmount(int newIncrement)
-//  {
-//    health += newIncrement;
-//    
-//    if (health > Constants.SHIP_STARTING_HEALTH)
-//    {
-//      health = Constants.SHIP_STARTING_HEALTH;
-//    }
-//  }
-//
-//  public void decrementHealthAmount()
-//  {
-//    decrementHealthAmount(1);
-//  }
-//  
-//  public void decrementHealthAmount(int amount)
-//  {
-//    if (amount < 0)
-//    {
-//      return;
-//    }
-//    
-//    health -= amount;
-//  }
-//  
-//  public int getHealthAmount()
-//  {
-//    return health;
-//  }
-//
-//  public boolean hasHealthRemaining()
-//  {
-//    return (health > 0 ? true : false);
-//  }
-
-//  /*
-//   * Methods regarding player's shields
-//   */
-//  public void incrementShieldAmount(int newIncrement)
-//  {
-//    shield += newIncrement;
-//    
-//    if (shield > Constants.SHIP_STARTING_SHIELD)
-//    {
-//      shield = Constants.SHIP_STARTING_SHIELD;
-//    }
-//  }
-//
-//  public void decrementShieldAmount()
-//  {
-//    decrementShieldAmount(1);
-//  }
-//  
-//  public void decrementShieldAmount(int amount)
-//  {
-//    if (amount < 0)
-//    {
-//      return;
-//    }
-//    
-//    shield -= amount;
-//  }
-//  
-//  public int getShieldAmount()
-//  {
-//    return shield;
-//  }
-//
-//  public boolean hasShieldRemaining()
-//  {
-//    return (shield > 0 ? true : false);
-//  }
-  
-//  /*
-//   * Methods regarding player's firepower
-//   */
-//  public int getCurrentFirepower()
-//  {
-//    return firePower;
-//  }
-//  
-//  public void reduceFirepower()
-//  {
-//    firePower--;
-//    
-//    if (firePower < Constants.SHIP_MIN_FIREPOWER)
-//    {
-//      firePower = Constants.SHIP_MIN_FIREPOWER;
-//    }
-//  }
-//  
-//  public void increaseFirepower()
-//  {
-//    firePower++;
-//    
-//    if (firePower > Constants.SHIP_MAX_FIREPOWER)
-//    {
-//      firePower = Constants.SHIP_MAX_FIREPOWER;
-//    }
-//  }
-  
-  /*
-   * Methods for the super shields
-   */
-//  public int getCurrentSuperShieldCount()
-//  {
-//    return superShield;
-//  }
-//  
-//  public void reduceSuperShield()
-//  {
-//    superShield--;
-//    
-//    if (superShield < 0)
-//    {
-//      superShield = 0;
-//      //superShield = Constants.SHIP_STARTING_SUPER_SHIELD;
-//    }
-//  }
-//
-//  // The player can have as many super shields as they keep collecting 
-//  public void increaseSuperShield()
-//  {
-//    superShield++;
-//  }
-
-  /*
-   * Power-ups for The Bomb
-   */
-//  public boolean hasTheBomb()
-//  {
-//    return ((theBomb > 0) ? true : false);
-//  }
-//  
-//  public void reduceTheBomb()
-//  {
-//    theBomb--;
-//    
-//    if (theBomb < 0)
-//    {
-//      theBomb = Constants.SHIP_STARTING_THE_BOMBS;
-//    }
-//  }
-//
-//  public int getTheBombCount()
-//  {
-//    return theBomb;
-//  }
-
-//  // The player can have as many super shields as they keep collecting 
-//  public void increaseTheBomb()
-//  {
-//    theBomb++;
-//  }
-  
   @Override
   public void kill()
   {
     // Don't quite kill the player yet if there is still health left
-    //if (health > 0)
     if (attributes.getValue(Constants.AttributeType.ATTRIBUTE_HEALTH) > 0)
     {
       return;
@@ -256,14 +89,4 @@ public class PlayerEntity extends EntityImage
     // A call to the parent method will clear both the visible and dead flags of the player entity.
     super.kill();
   }
-  
-//  private void initializeAttributes()
-//  {
-//    firePower = Constants.SHIP_STARTING_FIREPOWER;
-//    health = Constants.SHIP_STARTING_HEALTH;       
-//    shield = Constants.SHIP_STARTING_SHIELD;
-//    superShield = Constants.SHIP_STARTING_SUPER_SHIELD;
-//    theBomb = Constants.SHIP_STARTING_THE_BOMBS;
-    //autoShield = Constants.PLAYER_DEFAULT_AUTO_SHIELD_SETTING;
-//  }
 }
---
+++ src/galacticwarreboot/powerups/PowerupThrust.java
@@ -0,0 +1,19 @@
+package galacticwarreboot.powerups;
+
+import galacticwarreboot.Constants;
+import galacticwarreboot.entities.PowerupEntity;
+
+import java.awt.image.ImageObserver;
+
+public class PowerupThrust extends PowerupEntity
+{
+  public PowerupThrust(ImageObserver imageObserver)
+  {
+    super(imageObserver);
+
+    this.setPowerupType(Constants.PowerUpType.POWERUP_THRUST);
+    this.setAttributeType(Constants.AttributeType.ATTRIBUTE_THRUST);
+    this.setValue(Constants.SHIP_INCREASED_ACCELERATION);
+    //System.out.println("POWERUP Increased Thrust Spawned");
+  }
+}
